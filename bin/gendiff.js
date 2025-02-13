5fdae2246a38727ae09850e336be67ca573eee23
Tolkun Omurbekova <tolkun3922@gmail.com>
2025-02-13 13:41:21 +0600

    -updated gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,3 +5,6 @@ programm
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .version('1.0.0')
+    .option('-V, --version','output the version number')
+    .option('-h, --help','output usage information');
+programm.parse();
