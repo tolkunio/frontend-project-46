bc0e0900bcfd2dda102cf9df9763b378beebf00c
Tolkun Omurbekova <tolkun3922@gmail.com>
2025-02-24 13:32:22 +0600

    -add gendiff.test file

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,9 +7,10 @@ programm
     .name('gendiff')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
-    .arguments('<file1path1> <filepath2>')
+    .arguments('<filepath1> <filepath2>')
     .version('1.0.0')
     .action((filepath1,filepath2)=>{
         genDiff(filepath1,filepath2)
     })
 programm.parse(process.argv);
+
